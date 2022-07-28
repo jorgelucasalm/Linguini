36eec7039a45e957fc880e6bd169e703c7f325a9
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2022-07-28 11:13:39 -0300

    feat(database): leitura do arquivo csv

--- data/analysis/main.py
+++ data/analysis/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from dataset import *
-# dt = np.random.randn(10,20)
-dt = pd.read_csv("food_recipes", )
+
+dt = pd.read_csv("food_recipes.csv", ',') 
 st.title('Teste')
-st.dataframe(food_recipes.csv)
+st.dataframe(dt)
