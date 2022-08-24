9d1c273ebfeb1069a44cb41187de5cf3564c212e
Danielly Egito <egitodany@gmail.com>
2022-08-24 18:41:07 -0300

    fix: ajusta estilização dos inputs

--- lib/main.dart
+++ lib/main.dart
@@ -27,143 +27,183 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFEAF5F1),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Padding(padding: EdgeInsets.all(60)), //Espaçamento do topo
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Padding(
+                    padding: EdgeInsets.all(30)), //Espaçamento do topo
 
-              //LEMBRETE: Adicionar botão de voltar
+                //LEMBRETE: Adicionar botão de voltar
 
-              TextButton(
-                  onPressed: null,
-                  style: OutlinedButton.styleFrom(
-                    fixedSize: const Size(100, 32),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32),
+                TextButton(
+                    onPressed: null,
+                    style: OutlinedButton.styleFrom(
+                      fixedSize: const Size(100, 32),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Color(0xFF695876),
+                      ),
                     ),
-                    side: const BorderSide(
-                      width: 2,
-                      color: Color(0xFF695876),
-                    ),
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFF695876),
-                    ),
-                  ))
-            ],
-          ),
-          const Text(
-            'Cadastro',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w700,
-              color: Color(0xff422956),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFF695876),
+                      ),
+                    ))
+              ],
             ),
-          ),
-          const SizedBox(height: 32),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 2,
-                      color: Color(0xFF695876),
-                    ),
-                  ),
-                  focusColor: Color(0xFF695876),
-                  hintText: 'User',
-                ),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 2,
-                      color: Color(0xFF695876),
-                    ),
-                  ),
-                  hintText: 'E-mail',
-                ),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 2,
-                      color: Color(0xFF695876),
-                    ),
-                  ),
-                  hintText: 'Confirmação de e-mail',
-                ),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 2,
-                      color: Color(0xFF695876),
-                    ),
-                  ),
-                  hintText: 'Senha',
-                ),
-              )
-            ],
-          ),
-          const SizedBox(height: 32),
-          TextButton(
-              onPressed: null,
-              style: TextButton.styleFrom(
-                backgroundColor: const Color(0xffF5D950),
-                fixedSize: const Size(246, 48),
-                side: const BorderSide(
-                  width: 2,
+            const Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                "Cadastro",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
                   color: Color(0xff422956),
                 ),
+              ),
+            ),
+            const SizedBox(height: 32),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'User',
+                    labelStyle: const TextStyle(
+                      fontSize: 50,
+                      color: Color(0xFF695876),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF695876),
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    labelStyle: const TextStyle(
+                      fontSize: 50,
+                      color: Color(0xFF695876),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF695876),
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Confirmação de e-mail',
+                    labelStyle: const TextStyle(
+                      fontSize: 50,
+                      color: Color(0xFF695876),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF695876),
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Senha',
+                    labelStyle: const TextStyle(
+                      fontSize: 50,
+                      color: Color(0xFF695876),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF695876),
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          
+            const SizedBox(height: 32),
+            TextButton(
+                onPressed: null,
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color(0xffF5D950),
+                  fixedSize: const Size(246, 48),
+                  side: const BorderSide(
+                    width: 2,
+                    color: Color(0xff422956),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32)),
+                ),
+                child: const Text(
+                  'Cadastrar',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xff422956)),
+                )),
+
+            const SizedBox(height: 16),
+            OutlinedButton(
+              onPressed: null,
+              style: OutlinedButton.styleFrom(
+                fixedSize: const Size(246, 48),
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32)),
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                side: const BorderSide(
+                  width: 2,
+                  color: Color(0xFF695876),
+                ),
               ),
               child: const Text(
-                'Cadastrar',
+                'Já possuo conta',
                 style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w800,
-                    color: Color(0xff422956)),
-              )),
-          const SizedBox(height: 16),
-          TextButton(
-            onPressed: null,
-            style: OutlinedButton.styleFrom(
-              fixedSize: const Size(246, 48),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(32),
-              ),
-              side: const BorderSide(
-                width: 2,
-                color: Color(0xFF695876),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff422956),
+                ),
               ),
             ),
-            child: const Text(
-              'Já possuo conta',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w800,
-                color: Color(0xff422956),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
