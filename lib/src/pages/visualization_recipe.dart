a228009b9b3595e0e10162bd4380501e98576a99
Débora Silva <debora.raquel710@gmail.com>
2022-09-20 10:32:26 -0300

    fix: alterando o codigo de posicionamento da imagem da tela de visualizar receitas

--- lib/src/pages/visualization_recipe.dart
+++ lib/src/pages/visualization_recipe.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:linguini/src/components/header.dart';
 import 'package:linguini/src/components/button.dart';
-import 'package:linguini/src/components/sliver_image.dart';
-import 'package:linguini/src/components/cards_informativo.dart';
+import 'package:linguini/src/components/image_recipe.dart';
+import 'package:linguini/src/components/cards_information.dart';
 import 'package:linguini/src/components/list_ingredientes.dart';
 
 
@@ -23,60 +23,60 @@ class _VisualizationRecipeState extends State<VisualizationRecipe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          padding: const EdgeInsets.all(18),
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            children: [
-              const Header(),
-              Container(
-                  alignment: Alignment.centerLeft,
-                  child: Text("Receita",
-                      style: TextStyle(
-                        color: Color(0xff606060),
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Poppins',
-                      )
-                  )
-              ),
-              const Cards(),
-              Divider(
-                  height: 20,
-                  thickness: 2,
-                  color: Color(0xffD9D9D9)
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                  alignment: Alignment.centerLeft,
-                  child: Text("Ingredientes:",
-                      style: TextStyle(
-                        color: Color(0xff606060),
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Poppins',
-                      )
-                  )
-              ),
-              ListIngredientes(),
-              Container(
-                  padding: EdgeInsets.only(top: 15),
-                  alignment: Alignment.centerLeft,
-                  child: Text("Passo a passo:",
-                      style: TextStyle(
-                        color: Color(0xff606060),
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Poppins',
-                      )
-                  )
-              ),
-
-
-
-
-    ])));
+        body:
+        Container(
+              padding: const EdgeInsets.all(18),
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                  children: [
+                    const Header(),
+                    //ImageRecipe(),
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text("Receita",
+                            style: TextStyle(
+                              color: Color(0xff606060),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Poppins',
+                            )
+                        )
+                    ),
+                    const Cards(),
+                    Divider(
+                        height: 20,
+                        thickness: 2,
+                        color: Color(0xffD9D9D9)
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(top: 15),
+                        alignment: Alignment.centerLeft,
+                        child: Text("Ingredientes:",
+                            style: TextStyle(
+                              color: Color(0xff606060),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Poppins',
+                            )
+                        )
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(top: 15),
+                        alignment: Alignment.centerLeft,
+                        child: Text("Passo a passo:",
+                            style: TextStyle(
+                              color: Color(0xff606060),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Poppins',
+                            )
+                        )
+                    ),
+                  ]
+              )
+          )
+    );
   }
 
 }
