f6b061356143f313e5c7602371770d29352a5c11
Jorge Lucas <jorge.lucasalm@gmail.com>
2022-09-20 04:25:25 -0300

    feat: register page integration with API

--- lib/api.dart
+++ lib/api.dart
@@ -5,14 +5,25 @@ class Api {
   static const baseUrl = 'https://api-linguini.herokuapp.com';
   // static const baseUrl = 'http://192.168.1.7:3001';
 
-  static createUser(String username, String password, String email) async {
-    final response =
-        await http.post(Uri.parse(baseUrl), body: {username, password, email});
+  static Future<Map<String, dynamic>> createUser(
+      String username, String password, String email) async {
+    final response = await http.post(Uri.parse('$baseUrl/user'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'username': username,
+          'password': password,
+          'email': email
+        }));
 
-    print(response);
+    Map<String, dynamic> responseJson = jsonDecode(response.body);
+    final teste = response.statusCode;
+    return {'status': response.statusCode, 'message': responseJson['message']};
   }
 
-  static Future<Map<String, dynamic>> login(String username, String password) async {
+  static Future<Map<String, dynamic>> login(
+      String username, String password) async {
     final response = await http.post(Uri.parse('$baseUrl/user/login'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
@@ -21,8 +32,7 @@ class Api {
             <String, String>{'username': username, 'password': password}));
 
     Map<String, dynamic> responseJson = jsonDecode(response.body);
-    
-    return { 'status': response.statusCode, 'message': responseJson['message']};
 
+    return {'status': response.statusCode, 'message': responseJson['message']};
   }
 }
